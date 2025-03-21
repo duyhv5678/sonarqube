5b94c91273d43eb1e36eefb2bdcae76ddfa8612d
Duyhv <duyhv@vnext.vn>
2025-03-21 08:51:52 +0700

    Test errors

--- service/serivce-a/angular-tour-of-heroes/src/app/app.component.ts
+++ service/serivce-a/angular-tour-of-heroes/src/app/app.component.ts
@@ -7,8 +7,17 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-tour-of-heroes';
+  // Unused variable (should be detected)
+  unusedVar = 42;
 
   sum(a: number, b: number) {
     return a + b
+  }
+
+  // Potential bug: Weak equality check
+  checkValue() {
+    if ("123" == 123) { // Weak comparison
+      console.log("Values are equal")
+    }
   }
 }
